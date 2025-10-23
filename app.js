108ab155b39f02da8d389780d98436b2af47c28d
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-10-23 09:20:15 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const jokeElement = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+generateJoke();
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: "application/json",
+            },
+        }
+    try{
+        const res = await fetch('https://icanhazdadjoke.com/', config);
+        if(!res.ok) throw new Error(`Ajabaja! ${res.status}`);
+        const data = await res.json()
+        console.log(data)
+        jokeElement.innerHTML = data.joke
+    }catch (error){console.log(error)}
+};
+
+jokeBtn.addEventListener("click", generateJoke);
---
+++ pseudoAsync.js
@@ -0,0 +1,17 @@
+//grab joke & joke button
+
+//kalla på funktionen "generateJoke" 
+//  (för att det skall finnas ett skämt när sidan laddas)
+
+// skapa async funktion {
+//skapa variabeln "config"
+//ställ in att acceptera bara json från response
+
+
+//const variabel = try to await fetch
+//om response inte är -> fånga error
+    //await -> gör om till json
+
+//byt innerHTML till skämtet från json
+
+//}
